9087fb0f63e3acbe11390f5741289cb2456f9e1a
aspanoz <schnitzel@Schnitzel.local>
2015-08-24 11:32:14 +0400

    custom hundler

--- code/phasmatodea.model.js
+++ code/phasmatodea.model.js
@@ -1,19 +1,45 @@
 // Model for the timeline step sequencer
 
-// var global = new Global('phasmatodea') // Declare global variable
-setoutletassist(0, 'stepList'); // Tip for the outlet at the JS object
+outlets = 2; // set number of outlets
+setoutletassist(0, 'stepList');
+setoutletassist(1, 'stepListVisual');
 
 // Generation timeline long <timeAll> with the number of steps <maxStep> 
 function makeList(val) {
   var [maxStep, timeAll] = arrayfromargs(arguments);
+  var stepList = new Array(maxStep); // Array calculate intervals
+  var stepListVisual = new Array(maxStep); // Array calculate intervals for max/msp presentation mode
 
-	var size = (timeAll - timeAll % maxStep) / maxStep; // The size of the default time period
-	var remain = timeAll % maxStep; 
+  var size = (timeAll - timeAll % maxStep) / maxStep; // The size of the default time period
+  var remain = timeAll % maxStep; 
 
-	var stepList = new Array(maxStep); // Array calculate intervals
+  // 428 px - size of max/msp object in presentation mode
+  var sizeVisual = (428 - 428 % maxStep) / maxStep; // The size of the default time period
+  var remainVisual = 428 % maxStep; 
 
-	for(i = 0; i < maxStep; i++) {
-		stepList[i] = size + (remain > i);
+  for(i = 0; i < maxStep; i++) {
+    stepList[i] = size + (remain > i);
+    stepListVisual[i] = sizeVisual + (remainVisual > i);
   }
-	outlet(0, stepList);
+  outlet(0, stepList);
+  outlet(1, stepListVisual);
+}
+
+
+function loadStepList(val) {
+    var stepList = arrayfromargs(arguments);
+    // stepList = arrayfromargs(arguments);
+    var maxStep = stepList.pop();
+
+    var stepListVisual = new Array(maxStep);
+
+    var sizeVisual = (428 - 428 % maxStep) / maxStep; // The size of the default time period
+    var remainVisual = 428 % maxStep;
+
+    for(i = 0; i < maxStep; i++) {
+      stepListVisual[i] = sizeVisual + (remainVisual > i);
+    }
+
+    outlet(0, stepList);
+    outlet(1, stepListVisual);
 }
--- code/phasmatodea.view.js
+++ code/phasmatodea.view.js
@@ -1,32 +1,50 @@
 // View for the timeline step sequencer
 
-setoutletassist(0, 'viewMakeSteps') // Tip for the outlet at the JS object
+setoutletassist(0, 'stepListVisual'); // Tip for the outlet at the JS object
 
-// Building steps in the bpatcher
-function viewMakeSteps(val) {
+// Draw handle, use pictctrl object with file handle.png
+function makeHandle(obj, num, position) {
 
-	var remove = this.patcher.getnamed('steps')
-	if (remove) this.patcher.remove(remove);
+  var varname = 'handle' + num;
 
-  var bp = this.patcher.newdefault(0, 0, 'bpatcher', 'phasmatodea.steps.view.maxpat', '@presentation', 1, '@varname', 'steps')
-  var steps = this.patcher.getnamed('steps')
-  this.patcher.bringtofront(steps)
-
-  this.patcher.message('script', 'sendbox', 'steps', 'presentation_rect', 110, 30, 120, 40)
-
-  makeHandle(bp, 0)
-  makeHandle(bp, 1)
+  obj.message('script', 'newobject', 'newobj', '@text', 'pictctrl', '@varname', varname, '@presentation', 1);
+  obj.message('script', 'sendbox', varname, 'presentation_rect', position, 0, 13, 13);
+  obj.message('script', 'sendbox', varname, 'name', 'handle.png');
+  obj.message('script', 'sendbox', varname, 'mode', 1);
+  obj.message('script', 'sendbox', varname, 'snap', 1);
 }
 
-// Building handle, use pictctrl object with file handle.png
-function makeHandle(obj, num) {
+//Draw bpatcher, set bpatche script name
+function makeBpatcher() {
+  var varname = 'steps'; // bpatcher script name
 
-	var varname = 'handle_' + num
-	var position = num * 20
+  var bpatcher = this.patcher.getnamed(varname); // drop old bpatcher
+  if (bpatcher) {
+    this.patcher.remove(bpatcher);
+  }
 
-	obj.message('script', 'newobject', 'newobj', '@text', 'pictctrl', '@varname', varname, '@presentation', 1)
-	obj.message('script', 'sendbox', varname, 'presentation_rect', position, 13, 13, 13)
-	obj.message('script', 'sendbox', varname, 'name', 'handle.png')
-	obj.message('script', 'sendbox', varname, 'mode', 1)
-	obj.message('script', 'sendbox', varname, 'snap', 1)
+  var bp = this.patcher.newdefault(0, 0, 'bpatcher', 'phasmatodea.steps.view.maxpat', '@presentation', 1, '@varname', varname);
+  bpatcher = this.patcher.getnamed(varname);
+
+  this.patcher.bringtofront(bpatcher);
+  this.patcher.message('script', 'sendbox', varname, 'presentation_rect', 8, 30, 435, 40);
+
+  return bp;
+}
+
+// Building steps in the bpatcher
+function viewMakeSteps() {
+
+  var bp = makeBpatcher();
+
+  var sum = 0;
+  var list = arrayfromargs(arguments);
+
+  function createHandles(element, index) {
+    sum = sum + Number(element);
+    makeHandle(bp, index, sum);
+  }
+
+  list.pop(); // drop last element
+  list.forEach(createHandles);
 }
